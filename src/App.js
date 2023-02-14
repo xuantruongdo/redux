663b448397448cdb8b852e8b52e3b3b3ced48301
xuantruongdo <dev.doxuantruong@gmail.com>
2023-02-14 12:19:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Divider } from 'antd';
+import Filter from './components/filter/Filter';
+import AddTodo from './components/addTodo/AddTodo';
+
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: 500,
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        padding: 20,
+        boxShadow: '0 0 10px 4px #bfbfbf',
+        borderRadius: 5,
+        height: '90vh',
+      }}
+    >
+      <Title style={{ textAlign: 'center' }}>TODO APP with REDUX</Title>
+      <Filter />
+      <Divider/>
+      <AddTodo/>
     </div>
   );
 }
